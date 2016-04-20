a46ccc9208b36a1fb96237089e1fd635480025b2
gajotres100 <pongo666>
2016-04-20 21:18:12 +0200

    ae

---
+++ PotapanjeBrodova/Topništvo.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace PotapanjeBrodova
+{
+    public class Topništvo
+    {
+        public Rezultatgađanja UputiPucanj(Flota flota)
+        {
+
+            throw new NotImplementedException();
+        }
+
+        public void ObradirezultatGađanja(Rezultatgađanja rezultat)
+        {
+
+        }
+    }
+}
