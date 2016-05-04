4c63c23c29482ed2ae4441b1f9157eaf219ef290
gajotres100 <pongo666>
2016-05-04 20:02:09 +0200

    4.5.2016

---
+++ PotapanjeBrodova/Napipač.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace PotapanjeBrodova
+{
+    public class Napipač : IPucać
+    {
+        public Napipač(Mreža mreža, int duljinaBroda)
+        {
+            this.mreža = mreža;
+            this.duljinaBroda = duljinaBroda;
+        }
+        public Polje UputiPucanj()
+        {
+            List<Polje> polja = DajKandidateZaHorizontalniBrod().ToList();
+            polja.AddRange(DajKandidateZaVertikalniBrod());
+
+            int index = slučajno.Next(0, polja.Count());
+            return polja[index];
+        }
+
+        private Mreža mreža;
+        private int duljinaBroda;
+        private Random slučajno = new Random();
+
+
+        public IEnumerable<Polje> DajKandidateZaHorizontalniBrod()
+        {
+            List<Polje> kadidati = new List<Polje>();
+            var slobodna = mreža.DajSlobodnaPolja();
+
+            for (int r = 0; r < mreža.Redaka; ++r )
+            {
+                int brojacPolja = 0;
+                for(int s = 0; s < mreža.Stupaca; ++s)
+                {
+                    Polje p = new Polje(r, s);
+                    if (slobodna.Contains(p))
+                    {
+                        ++brojacPolja;
+                    }
+                    else
+                    {
+                        brojacPolja = 0;
+                    }
+                    if(brojacPolja >= duljinaBroda)
+                    {
+                        for (int ss = s - duljinaBroda; ss < s; ++ss)
+                            kadidati.Add(new Polje(r, ss));
+                    }
+                }
+            }
+
+             return kadidati;
+        }
+
+        public IEnumerable<Polje> DajKandidateZaVertikalniBrod()
+        {
+            List<Polje> kadidati = new List<Polje>();
+            var slobodna = mreža.DajSlobodnaPolja();
+
+            for (int s = 0; s < mreža.Stupaca; ++s)
+            {
+                int brojacPolja = 0;
+                for (int r = 0; r < mreža.Redaka; ++r)
+                {
+                    Polje p = new Polje(r, s);
+                    if (slobodna.Contains(p))
+                    {
+                        ++brojacPolja;
+                    }
+                    else
+                    {
+                        brojacPolja = 0;
+                    }
+                    if (brojacPolja >= duljinaBroda)
+                    {
+                        for (int rr = r - duljinaBroda + 1; rr <= r; ++rr)
+                            kadidati.Add(new Polje(rr, s));
+                    }
+                }
+            }
+
+            return kadidati;
+        }
+    }
+}
---
+++ UnitTests/TestNapipač.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using PotapanjeBrodova;
+using System.Linq;
+
+namespace UnitTests
+{
+    [TestClass]
+    public class TestNapipač
+    {
+        [TestMethod]
+        public void Napipač_ListaPpoljaZaHorizontalniBrodDuljinetriMoraSadržavati15Polja()
+        {
+            Mreža m = new Mreža(1, 7);
+            int duljinaBroda = 3;
+            Napipač n = new Napipač(m, duljinaBroda);
+            Assert.AreEqual(15, n.DajKandidateZaHorizontalniBrod().Count());
+        }
+
+        //[TestMethod]
+        //public void Napipač_ListaPpoljaZaHorizontalniBrodDuljinetriMoraSadržavatiPoljaUOdređenomBroju()
+        //{
+        //    Mreža m = new Mreža(1, 7);
+        //    int duljinaBroda = 3;
+        //    Napipač n = new Napipač(m, duljinaBroda);
+        //    Assert.AreEqual(15, n.DajKandidateZaHorizontalniBrod().Count());
+        //}
+
+        [TestMethod]
+        public void Napipač_ListaPpoljaZaVertilkalniBrodDuljineCetriMoraSadržavati15Polja()
+        {
+            Mreža m = new Mreža(5, 2);
+            int duljinaBroda = 4;
+            Napipač n = new Napipač(m, duljinaBroda);
+            Assert.AreEqual(15, n.DajKandidateZaVertikalniBrod().Count());
+        }
+    }
+}
