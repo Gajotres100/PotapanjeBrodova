5a71a57dd0c5f491b63ca486abbd12c4efe9a831
gajotres100 <pongo666>
2016-05-11 20:54:53 +0200

    završi kružnog pucaća

--- PotapanjeBrodova/KružniPucać.cs
+++ PotapanjeBrodova/KružniPucać.cs
@@ -10,14 +10,33 @@ namespace PotapanjeBrodova
         public KružniPucać(Polje prvoPogođeno, Mreža m)
         {
             this.PrvoPogođeno = prvoPogođeno;
-            this.mreža = mreža;
+            this.mreža = m;
         }
         public Polje UputiPucanj()
         {
-            throw new NotImplementedException();
+            int redak = PrvoPogođeno.Redak;
+            int stupac = PrvoPogođeno.Stupac;
+
+            List<IEnumerable<Polje>> kandidati = new List<IEnumerable<Polje>>();
+
+            foreach (Smjer smjer in Enum.GetValues(typeof(Smjer)))
+                mreža.DajPoljaUZadanomSmjeru(redak, stupac, smjer);
+            
+            kandidati.Sort((lista1, lista2) => lista2.Count() - lista1.Count());
+            var grupe = kandidati.GroupBy(lista => lista.Count());
+
+            var najdulji = grupe.First();
+
+            if(najdulji.Count() == 0)
+                return najdulji.First().First();
+
+            int indeks = slučajni.Next(0, najdulji.Count());
+
+            return najdulji.ElementAt(indeks).First();            
         }
 
         Polje PrvoPogođeno;
         Mreža mreža;
+        Random slučajni = new Random();
     }
 }
