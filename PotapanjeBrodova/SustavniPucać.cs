c096d6bc360359e9cd8684d8ad507b4bc3b87e78
gajotres100 <pongo666>
2016-05-11 21:01:40 +0200

    k

---
+++ PotapanjeBrodova/SustavniPucać.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace PotapanjeBrodova
+{
+    public class SustavniPucać : IPucać
+    {
+        public SustavniPucać(IEnumerable<Polje> pogođena, Mreža mreža)
+        {
+            pogođenaPolja = new List<Polje>(pogođena);
+            this.mreža = mreža;
+        }
+
+        List<Polje> pogođenaPolja;
+        Mreža mreža;
+    }
+}
